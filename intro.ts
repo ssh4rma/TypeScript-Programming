c31430131b6d26be85d97932c92e74e9cc899cab
Shubham <shubham.sharma4390@gmail.com>
2025-05-05 16:38:48 +0530

    add loop to iterate over a readonly character array

--- intro.ts
+++ intro.ts
@@ -10,3 +10,10 @@ console.log(names);
 for(let i : number = 0; i < names.length; ++i) {
   console.log(names[i]);
 }
+
+const charArr: readonly string[] =  ['you', 'can', 'only', 'read', 'this'];
+console.log(charArr);
+
+for(let i : number = 0; i < charArr.length; ++i) {
+    console.log(charArr[i]);
+}
