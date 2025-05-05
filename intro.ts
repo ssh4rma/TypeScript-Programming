7d449f025c5733631d267f7a04944a1392723c70
Shubham <shubham.sharma4390@gmail.com>
2025-05-05 16:35:30 +0530

    string example of names and iterate over it

--- intro.ts
+++ intro.ts
@@ -4,3 +4,9 @@ function addNums(a, b): number {
 }
 
 console.log(addNums(2, 3));
+
+const names: string[] = ['s', 'h', 'u', 'b', 'h', 'a', 'm'];
+console.log(names); 
+for(let i : number = 0; i < names.length; ++i) {
+  console.log(names[i]);
+}
