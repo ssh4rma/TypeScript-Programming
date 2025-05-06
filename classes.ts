c24cd71984b9da7f9909686f135ff9c56dca1223
Shubham <shubham.sharma4390@gmail.com>
2025-05-06 11:10:07 +0530

    refactor Point class to include overloaded constructors and default values

--- classes.ts
+++ classes.ts
@@ -1,11 +1,19 @@
 class Point {
-  x: number;
-  y: number; 
-  constructor(x = 0, y = 0) {
-    this.x = x;
-    this.y = y;
+  x: number = 0;
+  y: number = 0; 
+  constructor(x: number, y: number);
+  constructor(xy: string);
+  constructor(x: string | number = 0, y : number = 0) {
+    if(typeof x === 'string') {
+      const[xVal, yVal] = x.split(',').map(Number);
+      this.x = xVal;
+      this.y = yVal;
+    } else {
+      this.x = x;
+      this.y = y;
+    }
   }
 }
 
-const pt = new Point();
+const pt = new Point("54,21");
 console.log(`X coordinate is ${pt.x} and Y coordinate is ${pt.y}`);
