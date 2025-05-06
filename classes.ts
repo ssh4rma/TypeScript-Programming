6339825d35617d62cc162bd0d49cb809f174e4cc
Shubham <shubham.sharma4390@gmail.com>
2025-05-06 10:32:32 +0530

    make x coordinate readonly in Point class

--- classes.ts
+++ classes.ts
@@ -1,5 +1,5 @@
 class Point {
-  x: number;
+  readonly x: number;
   y: number; 
   constructor(x, y) {
     this.x = x;
@@ -8,4 +8,5 @@ class Point {
 }
 
 const pt = new Point(5, 4);
+pt.x = 10; //will throw an error
 console.log(`X coordinate is ${pt.x} and Y coordinate is ${pt.y}`);
