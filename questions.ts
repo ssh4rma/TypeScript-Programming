e8fa3cfcc291993223f20226c4cdd9b9467a5c47
Shubham <shubham.sharma4390@gmail.com>
2025-05-06 11:47:59 +0530

    type alias in TS

--- questions.ts
+++ questions.ts
@@ -8,3 +8,10 @@ function f1(x: string | number): number {
 }
 
 console.log(f1('dfadf'));
+
+//Define a type alias for a union of 'success' | 'error' | 'loading'
+
+type testCase = 'success' | 'error' | 'loading';
+let success: testCase = 'success';
+let error: testCase = 'error';
+let loading: testCase = 'loading';
