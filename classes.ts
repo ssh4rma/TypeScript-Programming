367eacb8d2b60ebd4550359b33d0d0e22e0dd31b
Shubham <shubham.sharma4390@gmail.com>
2025-05-06 10:28:12 +0530

    refactor Point class to initialize x coordinate and add constructor

--- classes.ts
+++ classes.ts
@@ -1,7 +1,9 @@
 class Point {
-  x = 0;
-  y = 4;
+  x: number;
+  constructor(x) {
+    this.x = x;
+  }
 }
 
-const pt = new Point();
+const pt = new Point(5);
 console.log(`X coordinate is ${pt.x} and Y coordinate is ${pt.y}`);
