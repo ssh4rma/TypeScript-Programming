8a70c8a128cc6800e505c9d981e55e8194a5ba9d
Shubham <shubham.sharma4390@gmail.com>
2025-05-08 12:10:23 +0530

    implemented super() in classes

--- class.ts
+++ class.ts
@@ -1,6 +1,11 @@
 class Point {
-  x: number = 10;
-  y: number = 20;
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
 
   scale(n: number): void {
     this.x *= n;
@@ -8,8 +13,17 @@ class Point {
   }
 }
 
-const p1 = new Point();
-console.log(`${p1.x} and ${p1.y}`); 
-p1.scale(5);
-console.log(`${p1.x} and ${p1.y}`); 
+class coordinates extends Point {
+  z: number;
 
+  constructor(x: number, y: number, z: number) {
+    super(x, y); // Pass x, y to Point
+    this.z = z;
+  }
+}
+
+// Test
+const c1 = new coordinates(10, 20, 30);
+console.log(`${c1.x} and ${c1.y} and ${c1.z}`); // Output: 10 and 20 and 30
+c1.scale(5);
+console.log(`${c1.x} and ${c1.y} and ${c1.z}`); // Output: 50 and 100 and 30
