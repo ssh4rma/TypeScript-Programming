54f8434d0ff38dac1cd4364f39fa5fecb2fc32f4
Shubham <shubham.sharma4390@gmail.com>
2025-05-08 10:29:06 +0530

    enum example

---
+++ questions.ts
@@ -0,0 +1,13 @@
+// Create an enum for days of the week.
+
+enum Week {
+  Monday = 1, 
+  Tuesday = 2, 
+  Wednesday = 3, 
+  Thrusday = 4,
+  Friday = 5, 
+  Saturday = 6, 
+  Sunday = 7
+}
+
+console.log(Week);
