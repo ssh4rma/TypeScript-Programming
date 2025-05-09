bc4eb648397fc106b894219cabd6c8eb11c31339
Shubham <shubham.sharma4390@gmail.com>
2025-05-09 11:38:23 +0530

    append text to the file

--- file-ops/file.js
+++ file-ops/file.js
@@ -3,8 +3,14 @@ const path = require('path');
 
 const filePath = path.join(__dirname, 'example.txt');
 
-// 1. Write to a file
+//write to a file
 fs.writeFile(filePath, 'Hello, this is a sample text file.', (err) => {
   if (err) throw err;
-  console.log('✅ File created and written successfully.');
+  console.log('File created and written successfully.');
 });
+
+//append text to file
+fs.appendFile(filePath, '\nThis line is appended.', (err) => {
+  if(err) throw err;
+  console.log('Data is successfully appended'); 
+})
