018f010de12a17eb5e6a1cbb940e11be843326f4
Shubham <shubham.sharma4390@gmail.com>
2025-05-09 15:04:42 +0530

    class decorator

--- decorators.ts
+++ decorators.ts
@@ -17,3 +17,19 @@ class ExampleClass {
   @second()
   method() {}
 }
+
+//class decorator
+@sealed
+class BugReport {
+  type = 'report'; 
+  title: string; 
+
+  constructor(t: string) {
+    this.title = t;
+  }
+}
+
+function sealed(constructor: Function) {
+  Object.seal(constructor);
+  Object.seal(constructor.prototype);
+}
